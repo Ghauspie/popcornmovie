46358bd909a2ed87de5ab868c1a342f81917920c
Ghauspie <hauspie.guillaume@free.fr>
2021-12-30 14:18:22 +0100

    component movie card created

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+/* import { Movies } from '../models/movie.model'; */
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  movies:any=[];
+  public imgurl="https://image.tmdb.org/t/p/original";
+  private _movieListUrl="https://api.themoviedb.org/3/movie/550?api_key=9d8b48fb32540c5a9d149f413900ee04";
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
-
+/*   fetchMovie= async() => {
+    let moviesList=await fetch(this._movieListUrl).then(res =>res.json()).then(data=>this.movie=data);
+    console.log(moviesList,this.movie);
+  } */
   ngOnInit(): void {
+     /* this.getMovie();  */
+/*    let tesr=this._httpClient.get<Movies[]>(this._movieListUrl);
+   return tesr;
+   console.log(tesr); */
+
+  this.getMovie();
+
+  }
+  
+  getMovie(){
+    this.httpClient.get<any>('https://api.themoviedb.org/3/movie/550?api_key=9d8b48fb32540c5a9d149f413900ee04').subscribe((Response: any)=>{
+      console.log(Response);
+     this.movies=Response; 
+    });
     
   }
 
