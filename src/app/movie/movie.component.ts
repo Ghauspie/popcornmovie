d794a2005d1f52f6d4852ebb7e92989c50268a86
unknown <hauspie.guillaume@free.fr>
2022-01-02 22:54:28 +0100

    dynamic display result

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -31,13 +31,18 @@ export class MovieComponent implements OnInit {
     console.log(this.SearchMovie);
     let find:string=this.SearchMovie;
     this.getMovie(find);
+
   }  
   getMovie(inputSearch:string):void{
+   
     let inputSearchClean:string = inputSearch.replace(/ /g,"+");
     this.httpClient.get<any>('http://api.themoviedb.org/3/search/multi?api_key=9d8b48fb32540c5a9d149f413900ee04&query='+inputSearchClean).subscribe((Response: any)=>{
       console.log(Response.results);
      this.movies=Response.results; 
      this.inputSearch=inputSearch;
+     let dynamic:any=document.querySelector('.dynamic')
+     dynamic.removeAttribute('class');
+     dynamic.setAttribute('class','dynamic');
     });
   }
 
